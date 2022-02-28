a308f497c8b84a5ae7b2c7797ae29eb9c24a3f13
Sultan Mahmud Ekram <smahmud1098@gmail.com>
2022-03-01 01:21:33 +0600

    card

--- app.js
+++ app.js
@@ -7,14 +7,35 @@ searchBtn.addEventListener("click", () => {
     fetch(` https://openapi.programming-hero.com/api/phones?search=${searchValue}`)
         .then(res => res.json())
         .then(data => display(data.data))
-
+        
 })
 
 const display = phones => {
-
+    const displayContainer = document.getElementById('display-container')
+    displayContainer.textContent=''
     phones.map(phone => {
 
-       console.log(phone)
+        
+
+        const card = document.createElement('div')
+       
+        card.innerHTML = ` <div class="col">
+                    <div class="card h-100">
+                        <img src="${phone.image}" class="card-img-top p-4 w-50 mx-auto" alt="...">
+                        <div class="card-body d-flex justify-content-center">
+                            <h5 class="card-title p-2 text-cener">${phone.brand}</h5>
+                            <p class="card-text p-2"> ${phone.phone_name}</p>
+                        </div>
+                        <div class="d-grid gap-2">
+                        <button class="btn btn-outline-primary" type="button">Explore More</button>
+                       
+                      </div>
+                    </div>
+                </div>`
+
+        displayContainer.append(card)
+
+        document.getElementById('search-field').value=''
     })
   
 }
