995f7fd98736f1dc95ff6c6dc4eed4404cae3641
Sultan Mahmud Ekram <smahmud1098@gmail.com>
2022-03-01 00:55:48 +0600

    data loaded

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("js file added")
+const searchBtn = document.getElementById("search-btn")
+
+
+searchBtn.addEventListener("click", () => {
+    const searchValue = document.getElementById('search-field').value
+    
+    fetch(` https://openapi.programming-hero.com/api/phones?search=${searchValue}`)
+        .then(res => res.json())
+        .then(data => display(data.data))
+
+})
+
+const display = phones => {
+
+    phones.map(phone => {
+
+       console.log(phone)
+    })
+  
+}
