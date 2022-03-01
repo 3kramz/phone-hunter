88cec382bff992016d9ca8c88aa2d636ec465bd4
Sultan Mahmud Ekram <smahmud1098@gmail.com>
2022-03-01 20:58:01 +0600

    card updated

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 const searchBtn = document.getElementById("search-btn")
 
-
 searchBtn.addEventListener("click", () => {
     const searchValue = document.getElementById('search-field').value
-    
     fetch(` https://openapi.programming-hero.com/api/phones?search=${searchValue}`)
         .then(res => res.json())
-        .then(data => display(data.data))
-        
+        .then(data => displayCard(data.data))
 })
 
-const display = phones => {
+const displayCard = data => {
     const displayContainer = document.getElementById('display-container')
-    displayContainer.textContent=''
+    displayContainer.textContent = ''
+    const phones = data.slice(0, 20)
+
     phones.map(phone => {
-
-        
-
         const card = document.createElement('div')
-       
-        card.innerHTML = ` <div class="col">
+        card.innerHTML = `   
+                <div class="col">
                     <div class="card h-100">
                         <img src="${phone.image}" class="card-img-top p-4 w-50 mx-auto" alt="...">
                         <div class="card-body d-flex justify-content-center">
@@ -27,15 +23,22 @@ const display = phones => {
                             <p class="card-text p-2"> ${phone.phone_name}</p>
                         </div>
                         <div class="d-grid gap-2">
-                        <button class="btn btn-outline-primary" type="button">Explore More</button>
+                        <a href="#display-data" onclick="phoneDetails('${phone.slug}')" class="btn btn-outline-primary"> Explore More  </a>
                        
                       </div>
                     </div>
                 </div>`
 
         displayContainer.append(card)
-
-        document.getElementById('search-field').value=''
+        document.getElementById('search-field').value = ''
+        document.getElementById("display-data").textContent=''
     })
-  
+
+}
+const phoneDetails = slug => {
+
+    fetch(`https://openapi.programming-hero.com/api/phone/${slug}`)
+        .then(resp => resp.json())
+        .then(data => console.log(data.data))
+      
 }
